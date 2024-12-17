cbd04c462ca9be028aa7b63718f2325eafda28a4
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-12-16 22:06:27 -0300

    feat: 259. Validating inpud upon form submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,27 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
   const email = useRef();
   const password = useRef();
 
+  const [emailIsInvalid, setEmailIsInvalid] = useState();
+
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(email.current.value, password.current.value);
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("Sending HTTP request...");
   }
 
   return (
@@ -17,7 +31,10 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input ref={email} id="email" type="email" name="email" />
+          <input ref={email} id="email" type="text" name="email" />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
